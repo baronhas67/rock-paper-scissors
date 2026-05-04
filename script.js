7b1f29e5215c45d9e842303e26e42b0f023d52c3
Bhagath <baronhas67@gmail.com>
2026-05-04 14:28:02 +0530

    Make a function that outputs rock, paper orscissor
    Using math.floor and math.random generate a random
    number from 1 to 3 and accordingly output rock,
    paper and scissor. Nothing is added to html.

---
+++ script.js
@@ -0,0 +1,21 @@
+// Create a function named getComputerChoice;
+function getComputerChoice() {
+    
+/* Use the Math.random to generate a random number, then put it inside the 
+# Math.floor to round it, and then multiply the randomly generated number    
+# with 3 inside the Math.floor function then add 1.
+
+# Store the randomly generated number from the above sequence inside a variable
+# called randomNumber. */
+
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+if (randomNumber === 1) {
+    return "Rock";
+} else if (randomNumber === 2) {
+    return "Paper";
+} else {
+    return "Scissor";
+}
+
+}
