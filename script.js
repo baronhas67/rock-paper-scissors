fa178fbbb01753b1aa3e76f16820df350b61c60a
Bhagath <baronhas67@gmail.com>
2026-05-04 15:38:08 +0530

    Create a new function that plays a single round
    Created a new function that invokes the gethuman-
    Choice and getComputerChoice function and compares
    them using switch conditionals and adds a point to
    the winner and also logs a message about the
    winner.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Create a function named getComputerChoice;
 function getComputerChoice() {
     
@@ -10,13 +13,15 @@ function getComputerChoice() {
 
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 
+console.log(randomNumber);
 if (randomNumber === 1) {
-    return "Rock";
+    return "rock";
 } else if (randomNumber === 2) {
-    return "Paper";
+    return "paper";
 } else {
-    return "Scissor";
+    return "scissor";
 }
+
 
 }
 
@@ -24,8 +29,62 @@ if (randomNumber === 1) {
 function getHumanChoice() {
 
     const choice = prompt("Choose between rock, paper and scissor!, (all lowercase)", "");
-    return choice;
+    const apChoice = choice.toLowerCase();
+    return apChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+console.log(humanChoice);
 
+    switch (humanChoice + "-" + computerChoice) {
+
+        case "rock-rock":
+            console.log("You both chose the same thing! rock. No points!");
+            break;
+
+        case "paper-paper":
+            console.log("You both chose the same thing! paper. No points!");
+            break;
+
+        case "scissor-scissor":
+            console.log("You both chose the same thing! scissor. No points!");
+            break;
+
+        case "rock-scissor":
+            console.log("You Win! Rock beats Scissor.");
+            humanScore++;
+            break;
+
+        case "paper-rock":
+            console.log("You Win! Paper beats Rock.");
+            humanScore++;
+            break;
+
+        case "scissor-paper":
+            console.log("You Win! Scissor beats Paper.");
+            humanScore++;
+            break;
+
+        case "rock-paper":
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+            break;
+
+        case "paper-scissor":
+            console.log("You lose! Scissor beats paper.");
+            computerScore++;
+            break;
+
+        case "scissor-rock":
+            console.log("You lose! Rock beats scissor");
+            computerScore++;
+            break;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
